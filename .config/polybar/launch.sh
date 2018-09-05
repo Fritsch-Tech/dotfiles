c67d896598dd5b4185d91e300d451c97e8ba9593
Lukas Fritsch <lukas.fritsch30@gmail.com>
2018-09-05 21:45:56 +0200

    Customisation

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bottom bar on all monitors
+for i in $(polybar -m | awk -F: '{print $1}');
+do MONITOR=$i polybar bottom & done
+
+
+
+echo "Bars launched..."
